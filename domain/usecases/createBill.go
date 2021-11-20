ded8dde4815d78bd861470573dcaa04b49c4c72c
viviRamos <financeiro@portalautopecas.com.br>
2021-11-20 10:23:42 -0300

    wip: refactored to update account in transfers and bill

--- domain/usecases/createBill.go
+++ domain/usecases/createBill.go
@@ -1,7 +1,13 @@
 package usecases
 
 import (
+	"errors"
+
 	"github.com/Vivi3008/apiTestGolang/domain"
+)
+
+var (
+	ErrValueEmpty = errors.New("Value is empty")
 )
 
 //cria o pagamento e atualiza a conta
@@ -12,7 +18,7 @@ func (a Accounts) CreateBill(bill domain.Bill) (domain.Bill, error) {
 		return domain.Bill{}, err
 	}
 
-	_, err = a.VerifyAccount(domain.AccountId(bill.AccountId), pay.Value)
+	_, err = a.VerifyAccount(domain.AccountId(bill.AccountId), pay.Value, true)
 
 	if err != nil {
 		return domain.Bill{}, err
@@ -22,18 +28,23 @@ func (a Accounts) CreateBill(bill domain.Bill) (domain.Bill, error) {
 }
 
 // verifica se a conta tem saldo, e atualiza a conta
-func (a Accounts) VerifyAccount(accountId domain.AccountId, value float64) (domain.Account, error) {
+func (a Accounts) VerifyAccount(accountId domain.AccountId, value float64, debit bool) (domain.Account, error) {
 	acc, err := a.ListAccountById(accountId)
+	var actualBalance float64
 
 	if err != nil {
 		return domain.Account{}, err
 	}
 
-	if acc.Balance < value {
-		return domain.Account{}, ErrInsufficientLimit
+	if debit {
+		actualBalance, err = debitFromAccount(acc.Balance, value)
+	} else {
+		actualBalance, err = creditToAccount(acc.Balance, value)
 	}
 
-	actualBalance := acc.Balance - value
+	if err != nil {
+		return domain.Account{}, err
+	}
 
 	updateAcc := domain.Account{
 		Id:        acc.Id,
@@ -43,7 +54,6 @@ func (a Accounts) VerifyAccount(accountId domain.AccountId, value float64) (doma
 		Secret:    acc.Secret,
 		CreatedAt: acc.CreatedAt,
 	}
-
 	err = a.store.StoreAccount(updateAcc)
 
 	if err != nil {
@@ -51,4 +61,23 @@ func (a Accounts) VerifyAccount(accountId domain.AccountId, value float64) (doma
 	}
 
 	return updateAcc, nil
+}
+
+func debitFromAccount(balance float64, value float64) (float64, error) {
+	if balance < value {
+		return 0, ErrInsufficientLimit
+	}
+	actualBalance := balance - value
+
+	return actualBalance, nil
+}
+
+func creditToAccount(balance float64, value float64) (float64, error) {
+	if value <= 0 {
+		return 0, ErrValueEmpty
+	}
+
+	actualBalance := balance + value
+
+	return actualBalance, nil
 }
--- domain/usecases/createTransfer.go
+++ domain/usecases/createTransfer.go
@@ -12,56 +12,17 @@ var (
 )
 
 func (a Accounts) CreateTransfer(trans domain.Transfer) (domain.Transfer, error) {
-	accountOrigin, err := a.ListAccountById(domain.AccountId(trans.AccountOriginId))
+	accountOrigin, err := a.VerifyAccount(domain.AccountId(trans.AccountOriginId), trans.Amount, true)
 
 	if err != nil {
 		return domain.Transfer{}, ErrIdOriginNotExists
 	}
 
-	accountDestination, err := a.ListAccountById(domain.AccountId(trans.AccountDestinationId))
-
-	if err != nil {
-		return domain.Transfer{}, ErrIdDestinyNotExists
-	}
-
-	if accountOrigin.Balance < trans.Amount {
-		return domain.Transfer{}, ErrInsufficientLimit
-	}
-
-	balanceOrigin := accountOrigin.Balance - trans.Amount
-	balanceDestiny := accountDestination.Balance + trans.Amount
-
-	updateAccOrigin := domain.Account{
-		Id:      string(accountOrigin.Id),
-		Name:    accountOrigin.Name,
-		Cpf:     accountOrigin.Cpf,
-		Secret:  accountOrigin.Secret,
-		Balance: balanceOrigin,
-	}
-
-	err = a.store.StoreAccount(updateAccOrigin)
-
-	if err != nil {
-		return domain.Transfer{}, err
-	}
-
-	updateAccDestiny := domain.Account{
-		Id:      string(accountDestination.Id),
-		Name:    accountDestination.Name,
-		Cpf:     accountDestination.Cpf,
-		Secret:  accountDestination.Secret,
-		Balance: balanceDestiny,
-	}
-
-	err = a.store.StoreAccount(updateAccDestiny)
-
-	if err != nil {
-		return domain.Transfer{}, err
-	}
+	accountDestination, err := a.VerifyAccount(domain.AccountId(trans.AccountDestinationId), trans.Amount, false)
 
 	transferOk := domain.Transfer{
-		AccountOriginId:      string(trans.AccountOriginId),
-		AccountDestinationId: string(trans.AccountDestinationId),
+		AccountOriginId:      string(accountOrigin.Id),
+		AccountDestinationId: string(accountDestination.Id),
 		Amount:               trans.Amount,
 	}
 
