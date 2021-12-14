3c5933e69fed68dbd65ad748b1bd4fe64be49ef9
Vivi Ramos <ramosluzviviane@gmail.com>
2021-12-14 15:57:36 -0300

    fix: return error

--- domain/usecases/createTransfer.go
+++ domain/usecases/createTransfer.go
@@ -1,24 +1,21 @@
 package usecases
 
 import (
-	"errors"
-
 	"github.com/Vivi3008/apiTestGolang/domain"
-)
-
-var (
-	ErrIdOriginNotExists  = errors.New("Account origin id doesn't exists")
-	ErrIdDestinyNotExists = errors.New("Account destiny id doesn't exists")
 )
 
 func (a Accounts) CreateTransfer(trans domain.Transfer) (domain.Transfer, error) {
 	accountOrigin, err := a.VerifyAccount(domain.AccountId(trans.AccountOriginId), trans.Amount, true)
 
 	if err != nil {
-		return domain.Transfer{}, ErrIdOriginNotExists
+		return domain.Transfer{}, err
 	}
 
 	accountDestination, err := a.VerifyAccount(domain.AccountId(trans.AccountDestinationId), trans.Amount, false)
+
+	if err != nil {
+		return domain.Transfer{}, err
+	}
 
 	transferOk := domain.Transfer{
 		AccountOriginId:      string(accountOrigin.Id),
