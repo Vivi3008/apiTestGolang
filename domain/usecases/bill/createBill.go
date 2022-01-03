13a514b1987bbf68046043b9be9cd12d193613e1
Vivi Ramos <ramosluzviviane@gmail.com>
2022-01-03 17:01:50 -0300

    feat: create bills usecases dir

--- domain/usecases/bill/createBill.go
+++ domain/usecases/bill/createBill.go
@@ -1,18 +1,19 @@
-package usecases
+package bill
 
 import (
 	"github.com/Vivi3008/apiTestGolang/domain/entities/bills"
+	"github.com/Vivi3008/apiTestGolang/domain/usecases/account"
 )
 
 //cria o pagamento e atualiza a conta
-func (a AccountUsecase) CreateBill(bill bills.Bill) (bills.Bill, error) {
+func (b BillUsecase) CreateBill(bill bills.Bill) (bills.Bill, error) {
 	pay, err := bills.NewBill(bill)
 
 	if err != nil {
 		return bills.Bill{}, err
 	}
 
-	_, err = a.VerifyAccount(bill.AccountId, pay.Value, Debit)
+	_, err = b.acRepo.UpdateAccountBalance(bill.AccountId, bill.Value, account.Debit)
 
 	if err != nil {
 		return bills.Bill{}, err
--- domain/usecases/bill/saveBill.go
+++ domain/usecases/bill/saveBill.go
@@ -1,11 +1,11 @@
-package usecases
+package bill
 
 import (
 	"github.com/Vivi3008/apiTestGolang/domain/entities/bills"
 )
 
-func (b Bills) SaveBill(bill bills.Bill) (bills.Bill, error) {
-	err := b.storeBill.StoreBill(bill)
+func (b BillUsecase) SaveBill(bill bills.Bill) (bills.Bill, error) {
+	err := b.blRepo.StoreBill(bill)
 
 	if err != nil {
 		return bills.Bill{}, err
