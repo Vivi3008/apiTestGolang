3c5f0196c163e8426076ae586d411e2500416615
Vivi Ramos <ramosluzviviane@gmail.com>
2022-01-10 10:59:19 -0300

    fix: remove comments

--- domain/usecases/account/login_test.go
+++ domain/usecases/account/login_test.go
@@ -93,37 +93,4 @@ func TestLogin(t *testing.T) {
 			}
 		})
 	}
-
-	/* t.Run("Shoul sign up successfuly", func(t *testing.T) {
-		accountStore := store.NewAccountStore()
-		accounts := CreateNewAccount(accountStore)
-
-		person := account.Account{
-			Name:    "Vanny",
-			Cpf:     "13323332555",
-			Secret:  "dafd33255",
-			Balance: 250000,
-		}
-
-		credentials := account.Login{
-			Cpf:    "13323332555",
-			Secret: "dafd33255",
-		}
-
-		account, err := accounts.CreateAccount(person)
-
-		if err != nil {
-			t.Fatal("Account should have been created successfully")
-		}
-
-		acc, err := accounts.NewLogin(credentials)
-
-		if err != nil {
-			t.Fatal("Login error")
-		}
-
-		if acc != account.Id {
-			t.Fatal("invalid Credentials")
-		}
-	}) */
 }
