e4a292e4d8346b1a46de397d5d82dadaf0ccea0f
Vivi Ramos <ramosluzviviane@gmail.com>
2022-01-19 11:30:45 -0300

    fix: refactor server

--- http/server.go
+++ http/server.go
@@ -8,40 +8,28 @@ import (
 	"github.com/Vivi3008/apiTestGolang/domain/usecases/transfers"
 	"github.com/Vivi3008/apiTestGolang/http/accounts"
 	"github.com/Vivi3008/apiTestGolang/http/auth"
+	bl "github.com/Vivi3008/apiTestGolang/http/bills"
 	"github.com/Vivi3008/apiTestGolang/http/middlewares"
+	transfer "github.com/Vivi3008/apiTestGolang/http/transfers"
+
 	"github.com/gorilla/mux"
 )
-
-type Server struct {
-	app account.AccountUsecase
-	tr  transfers.TranfersUsecase
-	bl  bill.BillUsecase
-	http.Handler
-}
 
 func NewServer(
 	accountUc account.AccountUsecase,
 	usecaseTr transfers.TranfersUsecase,
 	usecaseBl bill.BillUsecase,
-) Server {
-
-	server := Server{
-		tr: usecaseTr,
-		bl: usecaseBl,
-	}
-
+) http.Handler {
 	router := mux.NewRouter()
 	routerAuth := router.NewRoute().Subrouter()
 
 	accounts.NewHandler(router, accountUc)
 	auth.NewHandler(router, accountUc)
 
-	routerAuth.HandleFunc("/transfers", server.CreateTransfer).Methods((http.MethodPost))
-	routerAuth.HandleFunc("/bills", server.CreateBill).Methods((http.MethodPost))
-	routerAuth.HandleFunc("/bills", server.ListBills).Methods((http.MethodGet))
-	routerAuth.HandleFunc("/transfers", server.ListTransfer).Methods(http.MethodGet)
+	transfer.NewHandler(routerAuth, usecaseTr, accountUc)
+	bl.NewHandler(routerAuth, usecaseBl, accountUc)
+
 	routerAuth.Use(middlewares.Auth)
 
-	server.Handler = router
-	return server
+	return router
 }
