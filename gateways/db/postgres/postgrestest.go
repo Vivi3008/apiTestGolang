143b3d322e34d1233a6f06f959a868c53b427dbf
Vivi Ramos <ramosluzviviane@gmail.com>
2022-02-02 17:17:24 -0300

    feat: add dockertest

---
+++ gateways/db/postgres/postgrestest.go
@@ -0,0 +1,77 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+)
+
+func GetTestPool() (*pgxpool.Pool, func()) {
+	var db *pgxpool.Pool
+
+	dockerPool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	resource, err := dockerPool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "13.2-alpine",
+		Env: []string{
+			"POSTGRES_PASSWORD=secret",
+			"POSTGRES_USER=postgres",
+			"POSTGRES_DB=viviBank",
+			"listen_addresses = '*'",
+		},
+	}, func(hc *docker.HostConfig) {
+		hc.AutoRemove = true
+		hc.RestartPolicy = docker.RestartPolicy{Name: "no"}
+	})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	hostAndPort := resource.GetHostPort("5432/tcp")
+	databaseUrl := fmt.Sprintf("postgres://postgres:secret@%s/viviBank?sslmode=disable", hostAndPort)
+
+	log.Println("Connecting to database on url: ", databaseUrl)
+
+	resource.Expire(60) // Tell docker to hard kill the container in 60 seconds
+
+	dockerPool.MaxWait = 10 * time.Second
+	// connects to db in container, with exponential backoff-retry,
+	// because the application in the container might not be ready to accept connections yet
+	if err = dockerPool.Retry(func() error {
+		db, err = pgxpool.Connect(context.Background(), databaseUrl)
+
+		return err
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	migration, err := migrate.New(
+		"file://migrations",
+		databaseUrl)
+
+	if err != nil {
+		log.Fatalf("Could not read migration files: %s", err)
+	}
+
+	if err := migration.Up(); err != nil {
+		log.Fatal(err)
+	}
+
+	// tearDown should be called to destroy container at the end of the test
+	tearDown := func() {
+		db.Close()
+		dockerPool.Purge(resource)
+	}
+
+	return db, tearDown
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/Vivi3008/apiTestGolang
 go 1.16
 
 require (
+	github.com/containerd/continuity v0.2.2 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/docker/cli v20.10.12+incompatible // indirect
+	github.com/docker/docker v20.10.12+incompatible // indirect
 	github.com/golang-migrate/migrate/v4 v4.15.1 // indirect
 	github.com/google/uuid v1.3.0
 	github.com/gorilla/mux v1.8.0
@@ -13,5 +16,11 @@ require (
 	github.com/joho/godotenv v1.4.0
 	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 	github.com/lib/pq v1.10.4 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/opencontainers/runc v1.1.0 // indirect
+	github.com/ory/dockertest/v3 v3.8.1 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27 // indirect
 )
