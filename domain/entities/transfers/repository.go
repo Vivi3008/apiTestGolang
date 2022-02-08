169c5b542d2acc987455249cdbca7ab765668b73
Vivi Ramos <ramosluzviviane@gmail.com>
2022-02-08 16:41:53 -0300

    fix: return of select and variable names

--- domain/entities/transfers/repository.go
+++ domain/entities/transfers/repository.go
@@ -3,6 +3,6 @@ package transfers
 import "context"
 
 type TransferRepository interface {
-	ListTransfer(ctx context.Context, id string) ([]Transfer, error)
+	ListTransfer(ctx context.Context, originAccId string) ([]Transfer, error)
 	SaveTransfer(ctx context.Context, tr Transfer) error
 }
--- gateways/db/postgres/entries/transfers/list_tranfer.go
+++ gateways/db/postgres/entries/transfers/list_tranfer.go
@@ -6,12 +6,18 @@ import (
 	"github.com/Vivi3008/apiTestGolang/domain/entities/transfers"
 )
 
-func (r Repository) ListTransfer(ctx context.Context, id string) ([]transfers.Transfer, error) {
-	const statement = `SELECT * FROM transfers WHERE account_origin_id=$1`
+func (r Repository) ListTransfer(ctx context.Context, originAccId string) ([]transfers.Transfer, error) {
+	const statement = `SELECT 
+	id,
+	account_origin_id,
+	account_destination_id,
+	amount,
+	created_at
+	FROM transfers WHERE account_origin_id=$1`
 
 	var listTransfers = make([]transfers.Transfer, 0)
 
-	rows, err := r.Db.Query(ctx, statement, id)
+	rows, err := r.Db.Query(ctx, statement, originAccId)
 
 	if err != nil {
 		return []transfers.Transfer{}, err
