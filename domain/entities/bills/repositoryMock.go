a6cc0b326b124913261d31d063bffa426d38add9
Vivi Ramos <ramosluzviviane@gmail.com>
2022-02-09 11:05:00 -0300

    chore: add context in bills usecase

--- domain/entities/bills/repositoryMock.go
+++ domain/entities/bills/repositoryMock.go
@@ -1,14 +1,16 @@
 package bills
 
+import "context"
+
 type BillMock struct {
 	OnStore   func(b Bill) error
 	OnListAll func(id string) ([]Bill, error)
 }
 
-func (b BillMock) ListBills(id string) ([]Bill, error) {
+func (b BillMock) ListBills(ctx context.Context, id string) ([]Bill, error) {
 	return b.OnListAll(id)
 }
 
-func (b BillMock) StoreBill(bl Bill) error {
+func (b BillMock) StoreBill(ctx context.Context, bl Bill) error {
 	return b.OnStore(bl)
 }
--- domain/usecases/bill/save_bill.go
+++ domain/usecases/bill/save_bill.go
@@ -1,11 +1,13 @@
 package bill
 
 import (
+	"context"
+
 	"github.com/Vivi3008/apiTestGolang/domain/entities/bills"
 )
 
-func (b BillUsecase) SaveBill(bill bills.Bill) error {
-	err := b.blRepo.StoreBill(bill)
+func (b BillUsecase) SaveBill(ctx context.Context, bill bills.Bill) error {
+	err := b.blRepo.StoreBill(ctx, bill)
 	if err != nil {
 		return err
 	}
