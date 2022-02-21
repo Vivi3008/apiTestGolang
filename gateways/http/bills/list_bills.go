91471dc9903e8a58e6f579c2ccb05b8d0508ed57
Vivi Ramos <ramosluzviviane@gmail.com>
2022-02-21 18:02:10 -0300

    feat: add list bills tests in http

--- gateways/http/bills/list_bills.go
+++ gateways/http/bills/list_bills.go
@@ -19,13 +19,14 @@ func (h Handler) ListBills(w http.ResponseWriter, r *http.Request) {
 	list, err := h.blUse.ListBills(r.Context(), accountId)
 
 	if err != nil {
-		response.SendError(w, err, http.StatusBadRequest)
+		response.SendError(w, err, http.StatusInternalServerError)
 		return
 	}
 
 	payments := make([]BillReqRes, len(list))
 
 	for i, bill := range list {
+		payments[i].Id = bill.Id
 		payments[i].AccountId = bill.AccountId
 		payments[i].Description = bill.Description
 		payments[i].Value = bill.Value
