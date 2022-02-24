918bd6d45daf58e0c52f97a28421707d25f52291
Vivi Ramos <ramosluzviviane@gmail.com>
2022-02-24 18:26:32 -0300

    feat: add type transfer in file manager

--- gateways/db/store/file_manager.go
+++ gateways/db/store/file_manager.go
@@ -9,18 +9,21 @@ import (
 
 	"github.com/Vivi3008/apiTestGolang/domain/entities/account"
 	"github.com/Vivi3008/apiTestGolang/domain/entities/bills"
+	"github.com/Vivi3008/apiTestGolang/domain/entities/transfers"
 )
 
 type Entity string
 
 const (
-	accountType Entity = "account"
-	billType    Entity = "bill"
+	accountType  Entity = "account"
+	billType     Entity = "bill"
+	transferType Entity = "transfer"
 )
 
 type Entities struct {
-	Account []account.Account
-	Bill    []bills.Bill
+	Account  []account.Account
+	Bill     []bills.Bill
+	Transfer []transfers.Transfer
 }
 
 var ErrSaveInFile = errors.New("error to save in file")
@@ -49,7 +52,12 @@ func StoreFile(writeData interface{}, source string) error {
 func ReadFile(source string, typeEntitie Entity) (Entities, error) {
 	var accountData []account.Account
 	var billData []bills.Bill
+	var transferData []transfers.Transfer
 
+	err := os.Chmod(source, 0777)
+	if err != nil {
+		return Entities{}, err
+	}
 	dataJson, err := ioutil.ReadFile(source)
 
 	if err != nil {
@@ -63,6 +71,9 @@ func ReadFile(source string, typeEntitie Entity) (Entities, error) {
 	case billType:
 		json.Unmarshal(dataJson, &billData)
 		return Entities{Bill: billData}, nil
+	case transferType:
+		json.Unmarshal(dataJson, &transferData)
+		return Entities{Transfer: transferData}, nil
 	default:
 		return Entities{}, nil
 	}
