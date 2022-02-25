57a1a9acea554229dab6b004e8a735151932dfcb
Vivi Ramos <ramosluzviviane@gmail.com>
2022-02-25 15:24:26 -0300

    chore: add complete test case

--- gateways/db/store/activity/list_activity_test.go
+++ gateways/db/store/activity/list_activity_test.go
@@ -7,25 +7,76 @@ import (
 	"testing"
 
 	"github.com/Vivi3008/apiTestGolang/domain/usecases/activities"
+	"github.com/Vivi3008/apiTestGolang/gateways/db/store"
 )
+
+var sourceTestBill = "../bills/bills_test.json"
+var sourceTestTransfer = "../transfers/transfers_test.json"
 
 func TestListActivities(t *testing.T) {
 	type TestCase struct {
-		Name          string
-		args          string
-		runBeforeBill func() error
-		want          []activities.AccountActivity
-		err           error
+		Name              string
+		args              string
+		runBeforeBill     func(string, interface{}) error
+		runBeforeTransfer func(string, interface{}) error
+		sourceBill        string
+		sourceTransfer    string
+		want              []activities.AccountActivity
+		err               error
+	}
+
+	wantActitivies := []activities.AccountActivity{
+		{
+			Type:      activities.Bill,
+			Amount:    store.BillsTest[1].Value,
+			CreatedAt: store.BillsTest[1].ScheduledDate,
+			Details: activities.DescriptionPayment{
+				Description: store.BillsTest[1].Description,
+				Status:      store.BillsTest[1].StatusBill,
+			},
+		},
+		{
+			Type:      activities.Bill,
+			Amount:    store.BillsTest[2].Value,
+			CreatedAt: store.BillsTest[2].ScheduledDate,
+			Details: activities.DescriptionPayment{
+				Description: store.BillsTest[2].Description,
+				Status:      store.BillsTest[2].StatusBill,
+			},
+		},
+		{
+			Type:      activities.Transfer,
+			Amount:    store.TransfersTest[1].Amount,
+			CreatedAt: store.TransfersTest[1].CreatedAt,
+			Details: activities.DestinyAccount{
+				Name:                 store.AccountsTest[2].Name,
+				AccountDestinationId: store.TransfersTest[1].AccountDestinationId,
+			},
+		},
+		{
+			Type:      activities.Transfer,
+			Amount:    store.TransfersTest[2].Amount,
+			CreatedAt: store.TransfersTest[2].CreatedAt,
+			Details: activities.DestinyAccount{
+				Name:                 store.AccountsTest[1].Name,
+				AccountDestinationId: store.TransfersTest[2].AccountDestinationId,
+			},
+		},
 	}
 
 	testCases := []TestCase{
 		{
 			Name: "Should list activities succesffull",
-			args: BillsTest[0].AccountId,
-			runBeforeBill: func() error {
-				return CreateBillsActivities()
+			args: store.BillsTest[0].AccountId,
+			runBeforeBill: func(s string, i interface{}) error {
+				return store.CreateDataFile(s, i)
 			},
-			want: []activities.AccountActivity{},
+			runBeforeTransfer: func(s string, i interface{}) error {
+				return store.CreateDataFile(s, i)
+			},
+			sourceBill:     sourceTestBill,
+			sourceTransfer: sourceTestTransfer,
+			want:           wantActitivies,
 		},
 	}
 
@@ -33,18 +84,22 @@ func TestListActivities(t *testing.T) {
 		tt := tc
 		t.Run(tt.Name, func(t *testing.T) {
 			t.Cleanup(func() {
-				err := DeleteBillsActivities()
+				err := store.DeleteDataFile(tt.sourceBill)
 				if err != nil {
-					t.Errorf("err delelte bill test %s", err)
+					t.Errorf("err delete bill test %s", err)
 				}
 			})
 
 			if tt.runBeforeBill != nil {
-				tt.runBeforeBill()
+				tt.runBeforeBill(tt.sourceBill, store.BillsTest)
 			}
 
-			str := NewAccountAcitivity()
-			str.billStore.Src = "../bills/bills_test.json"
+			if tt.runBeforeTransfer != nil {
+				tt.runBeforeTransfer(tt.sourceTransfer, store.TransfersTest)
+			}
+
+			str := NewAccountActivity()
+			str.billStore.Src = tt.sourceBill
 
 			got, err := str.ListActivity(context.Background(), tt.args)
 
