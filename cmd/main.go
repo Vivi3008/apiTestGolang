ac2025ccf893fa5daf450aed2bcb4446d97202ed
Vivi Ramos <ramosluzviviane@gmail.com>
2022-03-03 11:54:46 -0300

    refact: change jwt lib version

--- cmd/main.go
+++ cmd/main.go
@@ -27,7 +27,6 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
 )
 
 func main() {
